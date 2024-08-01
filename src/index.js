eb9b7101af7b4363ec2b37a9dff8ee6ecbd5979f
SagaMS <91miablond@gmail.com>
2024-08-01 18:27:34 +0700

    feat:solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function toReadable (number) {
-  
-}
+ module.exports = function toReadable (number) {
+// function toReadable (number) {    
+    const zerotoNineteen = ["zero", "one",
+        "two", 'three', 'four', 'five', 'six', 'seven',
+        'eight', 'nine', 'ten', 'eleven', 'twelve',
+        'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 
+        'eighteen', 'nineteen'];
+
+    const dozens =[ 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        
+  if(number<=19){
+    return zerotoNineteen[number];
+  }else if(number<100){
+    if (number%10===0){
+        return dozens[(number/10)-2];
+    }else{
+        return  dozens[Math.floor((number/10)-2)]+ ' ' + zerotoNineteen[number%10];
+    }
+    }else{
+        if (number%100===0){
+        return zerotoNineteen[number/100] + ' hundred';
+    }else if(10<=(number%100) && (number%100)<=19){
+        return  zerotoNineteen[Math.floor(number/100)] + ' hundred ' + zerotoNineteen[number%100];
+        
+    }else if(number%10===0){
+        return  zerotoNineteen[Math.floor(number/100)] + ' hundred '+ dozens[(((number/10)%10)-2)];
+    }else{
+        if(((Math.floor(number/10))%10) ==0){
+            return zerotoNineteen[Math.floor(number/100)] + ' hundred ' + zerotoNineteen[number%100]
+        }else{
+
+            return zerotoNineteen[Math.floor(number/100)] + ' hundred '+ 
+            dozens[((Math.floor(number/10)%10)-2)] + " " + zerotoNineteen[(number%100)%10];
+    }
+    }
+    }
+  }
+
+
+
+
